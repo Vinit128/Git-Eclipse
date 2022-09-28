21aa4e4d89d907e46342265e4cab03965fc625e4
Vinit128 <patelvinit128@gmail.com>
2022-09-28 18:02:08 -0500

    testing

--- demo1-TestProject/src/main/java/com/example/demo/Demo1TestProjectApplication.java
+++ demo1-TestProject/src/main/java/com/example/demo/Demo1TestProjectApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Demo1TestProjectApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Demo1TestProjectApplication.class, args);
 	}
